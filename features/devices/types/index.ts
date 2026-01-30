6c5b815176e66b94a69bca1f7d0319b861e93379
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2026-01-30 14:57:48 -0300

    {feat] devices feature

---
+++ features/devices/types/index.ts
@@ -0,0 +1,32 @@
+
+export enum DeviceStatus {
+    ONLINE = 'ONLINE',
+    OFFLINE = 'OFFLINE'
+}
+
+export enum ProcessStage {
+    PANEL_ASSEMBLY = "MONTAGEM PAINEL",
+    BENCH_ASSEMBLY = "MONTAGEM BANCADA",
+    SOLDERING = "SOLDA",
+    QUALITY_CONTROL = "CONTROLE DE QUALIDADE",
+    PACKAGING = "EMBALAGEM",
+    TESTING = "TESTES"
+}
+
+export enum ProcessStatus {
+    IDLE = 'PARADO',
+    RUNNING = 'PRODUZINDO',
+    PAUSED = 'PAUSADO',
+}
+
+export interface DeviceDetails {
+    id: number;
+    name: string;
+    macAddress: string;
+    ipAddress: string;
+    lastSeen: string;
+    status: DeviceStatus;
+    processStage: string;
+    processStatus: string;
+    order?: string;
+}
